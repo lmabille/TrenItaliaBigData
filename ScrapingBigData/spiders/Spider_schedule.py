0a7e4d65db52e09a550c6fa8477e6491da12f9a1
Gregor Winkler <gwinkler@runbox.com>
2022-11-12 10:30:44 +0100

    feat(Spider_schedule): add new parse method

--- ScrapingBigData/spiders/Spider_schedule.py
+++ ScrapingBigData/spiders/Spider_schedule.py
@@ -34,7 +34,7 @@ class QuotesSpider(Spider):
         for station in self.data:
             link = f"{self.url}{station['id']}/{date}"
             url = f"http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/partenze/{station['id']}/{day}%20{month}%20{day_num}%20{year}%20{hour}%20GMT+0200%20(heure%20d%E2%80%99%C3%A9t%C3%A9%20d%E2%80%99Europe%20centrale)"
-            yield Request(url=url, callback=self.parse, meta={'station_departure': station['name'], 'id': station['id']})
+            yield Request(url=url, callback=self.returnRawData, meta={'station_departure': station['name'], 'id': station['id']})
 
     def parse(self, response):
         station_departure = response.meta['station_departure']
@@ -42,10 +42,39 @@ class QuotesSpider(Spider):
         if response.text != '[]':
             jsonresponse = json.loads(response.text)
             trains = trains_item()
+            # Why is arrival set to departure?
             trains['station_arrival'] = station_departure
             trains['station_arrival_id'] = station_departure_id
+            #
             for train in jsonresponse:
                 trains['train_id'] = train['numeroTreno']
                 trains['delay'] = train['compRitardo'][1]
                 trains['station_departure_id'] = train['codOrigine']
+                yield trains
+
+    # Only deleting unneccessary attributed like img paths
+    # Furthermore picking the english valuees from string arrays
+    def returnRawData(self, response):
+        station_departure = response.meta['station_departure']
+        station_departure_id = response.meta['id']
+        if response.text != '[]':
+            jsonresponse = json.loads(response.text)
+            trains = {}
+            trains['station_departure'] = station_departure
+            trains['station_departure_id'] = station_departure_id
+            for train in jsonresponse:
+                train['delay'] = train['compRitardo'][1]
+                train['departure_status'] = train['compInStazionePartenza'][1]
+                train['arrival_status'] = train['compInStazioneArrivo'][1]
+                del train['compRitardo']
+                del train['compInStazionePartenza']
+                del train['compInStazioneArrivo']
+                del train['compImgRitardo2']
+                del train['compImgRitardo']
+                del train['compClassRitardoTxt']
+                del train['compClassRitardoLine']
+                del train['compImgCambiNumerazione']
+                del train['compOrientamento']
+                del train['compRitardoAndamento']
+                trains.update(train)
                 yield trains
